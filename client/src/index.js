04c28f35d8a426893504b6248bb58085c4d991f4
Mehmet Eray Sürmeli <surmelimehmeteray@gmail.com>
2021-07-23 22:10:47 +0300

    Redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import {Provider} from "react-redux";
+import Store from "./store";
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={Store}>
+        <App />
+    </Provider>,
+    document.getElementById("root")
 );
+
