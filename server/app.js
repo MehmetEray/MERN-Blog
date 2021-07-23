a2e8e4ba9c4ee669a4ef43c7d164368c1d52e60f
Mehmet Eray Sürmeli <surmelimehmeteray@gmail.com>
2021-07-23 17:04:53 +0300

    redux and client developed

--- server/app.js
+++ server/app.js
@@ -1,41 +1,32 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import postRoutes from "./routes/posts.js";
+const app = express();
+dotenv.config();
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get("/", (req, res) => {
+  res.send("coding with didem .. :) Musmutlu yıllar !!");
 });
+app.use("/posts", postRoutes);
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+const PORT = process.env.PORT || 5000;
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+mongoose
+  .connect(process.env.con_url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`----------Server running on port: ${PORT}----------- `));
+  })
+  .catch((error) => {
+    console.error(error.message);
+  });
 
-module.exports = app;
+mongoose.set("useFindAndModify", false);
