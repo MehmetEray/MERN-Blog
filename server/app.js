f98f2e3466b90b5eb0b719eec8cf3f94b9016aec
Mehmet Eray Sürmeli <surmelimehmeteray@gmail.com>
2021-07-26 18:39:22 +0300

    modified server.js

--- server/app.js
+++ server/app.js
@@ -11,9 +11,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 const PORT = process.env.PORT || 5000;
 
-// app.get("/", (req, res) => {
-//     res.send("running");
-// });
 var con =
     "mongodb+srv://mehmeteray:mehmeteray123@cluster0.cpokm.mongodb.net/blogify?retryWrites=true&w=majority";
 mongoose.connect(con, { useUnifiedTopology: true, useNewUrlParser: true });
@@ -23,19 +20,7 @@ connection.once("open", function () {
     console.log("MongoDB database connection established successfully");
 });
 
-// mongoose
-//     .connect(process.env.con_url, {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true,
-//     })
-//     .then(() => {
-//         app.listen(PORT, () =>
-//             console.log(`----------Server running on port: ${PORT}----------- `)
-//         );
-//     })
-//     .catch((error) => {
-//         console.error(error.message);
-//     });
+
 app.use("/posts", postRoutes);
 app.listen(process.env.PORT, function () {
     console.log("Server is running on Port: " + process.env.PORT);
